d58dde894d35b51d890fd5dd3c7b5b74a39dcac1
edumntg <edumntg@gmail.com>
2015-02-27 14:28:07 -0430

    Arreglado Loot Droper

--- Scripts/Utils/Loot Drop.lua
+++ Scripts/Utils/Loot Drop.lua
@@ -1,5 +1,3 @@
-local bpsUsed = 3
-
 local items_str = {'plate armor', 'two-handed sword'}
 local items_id = {}
 
@@ -11,11 +9,11 @@ Module.New('Loot droper', function(module)
 
 	local corpse = Container.GetLast()
 	local pos = Self.Position()
-	if corpse:Index() > (bpsUsed - 1) then --not a backpack
-		if corpse:GetName():find('dead') then --container name has word 'dead' ie: dead orc
-			local items = corpse:iItems()
-			for spot, item in pairs(items) do
-				if table.contains(items_id, item.id) then --match!
+	if corpse:GetName():find('dead') then --container name has word 'dead' ie: dead orc
+		local items = corpse:iItems()
+		for spot, item in pairs(items) do
+			if table.contains(items_id, item.id) then --match!
+				if item:GetWeight() > Self.Cap() then
 					Walker.Delay(1000)
 					corpse:MoveItemToGround(spot, pos.x pos.y, pos.z, item:ItemCount())
 				end
