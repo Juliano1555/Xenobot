04638278a7d85c9244f5b0473b12eb4188f30fd0
edumntg <edumntg@gmail.com>
2015-02-27 14:25:15 -0430

    Agregado el Loot Droper

---
+++ Scripts/Utils/Loot Drop.lua
@@ -0,0 +1,28 @@
+local bpsUsed = 3
+
+local items_str = {'plate armor', 'two-handed sword'}
+local items_id = {}
+
+for _, n in ipairs(items_str) do
+	table.insert(items_id, Item.GetID(n))
+end
+
+Module.New('Loot droper', function(module)
+
+	local corpse = Container.GetLast()
+	local pos = Self.Position()
+	if corpse:Index() > (bpsUsed - 1) then --not a backpack
+		if corpse:GetName():find('dead') then --container name has word 'dead' ie: dead orc
+			local items = corpse:iItems()
+			for spot, item in pairs(items) do
+				if table.contains(items_id, item.id) then --match!
+					Walker.Delay(1000)
+					corpse:MoveItemToGround(spot, pos.x pos.y, pos.z, item:ItemCount())
+				end
+			end
+		end
+	end
+end)
+
+
+			
