04fe35eb40e7666d13e076542e0aa449da740b12
Juliano1555 <59325743+Juliano1555@users.noreply.github.com>
2019-12-29 00:49:48 -0300

    soft

--- Scripts/AutoEquipSoft.lua
+++ Scripts/AutoEquipSoft.lua
@@ -1,8 +1,8 @@
 local secondary = (Self.Name() == "Darkhaos" and Item.GetID("depth calcei") or Item.GetID("boots of haste")) --las botas normales
 local soft = 
 {
-	normal = 6529,
-	using = 3549
+	normal = 1500,
+	using = 500
 }
 while true do
 	if not Self.isInPz() then
